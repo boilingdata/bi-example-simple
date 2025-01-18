91643d2fffb72be82a3916afc251ec5f69bc1225
BoilingInsights <>
2025-01-18 15:02:55 +0200

    saveFile

--- sql-workbooks/testing-iceberg-avro.sql
+++ sql-workbooks/testing-iceberg-avro.sql
@@ -4,18 +4,23 @@ SELECT filename, "last-sequence-number" AS seq FROM read_json_auto('s3://athena-
 
 -- 1. Iceberg Table version file; it may contain multiple versions
 FROM read_json('s3://athena-results-dforsber/iceberg_table/metadata/00001-7ee2692b-2afe-4551-b11d-ffcae35c519e.metadata.json');
-SET VARIABLE manifest_list = (SELECT snapshots[2]['manifest-list'] AS path FROM read_json('s3://athena-results-dforsber/iceberg_table/metadata/00001-7ee2692b-2afe-4551-b11d-ffcae35c519e.metadata.json'));
-SELECT getvariable('manifest_list');
+SELECT snapshots[2]['manifest-list'] AS path FROM read_json('s3://athena-results-dforsber/iceberg_table/metadata/00001-7ee2692b-2afe-4551-b11d-ffcae35c519e.metadata.json')
+SET VARIABLE manifest_list_avro_file = (SELECT snapshots[2]['manifest-list'] AS path FROM read_json('s3://athena-results-dforsber/iceberg_table/metadata/00001-7ee2692b-2afe-4551-b11d-ffcae35c519e.metadata.json'));
+SELECT getvariable('manifest_list_avro_file') AS path;
 
 -- 2. snapshot manifest-list Avro file
-FROM read_avro('s3://athena-results-dforsber/iceberg_table/metadata/snap-1148784968388100414-1-ffe5b608-5643-4c41-a25d-e1c686d50a05.avro')
-FROM read_avro(getvariable('manifest_list'));
+FROM read_avro(getvariable('manifest_list_avro_file'));
+SELECT list(manifest_path) AS path FROM read_avro(getvariable('manifest_list'))
 SET VARIABLE manifests = ( SELECT list(manifest_path) AS path FROM read_avro(getvariable('manifest_list')));
 
 -- 3. the actual manifest Avro file
-FROM read_avro('s3://athena-results-dforsber/iceberg_table/metadata/ffe5b608-5643-4c41-a25d-e1c686d50a05-m0.avro')
 FROM read_avro(getvariable('manifests'));
 SELECT data_file['file_path'] FROM read_avro(getvariable('manifests'));
+
+-- 4. read the table data
+SET VARIABLE data_files = ( SELECT list(data_file['file_path']) AS path FROM read_avro(getvariable('manifests')) );
+
+
 
 
 --
