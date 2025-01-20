064eeb51107950e36ec636ec124f277abcbe5fcb
BoilingInsights <>
2025-01-20 11:34:55 +0200

    saveFile

---
+++ sql-workbooks/testing-iceberg-simpler.sql
@@ -0,0 +1,13 @@
+INSTALL avro FROM community;
+LOAD avro;
+
+-- 0. Figure out all Iceberg Table versions (through all manifest files)
+SET VARIABLE metadata_json_path = ( SELECT filename FROM read_json_auto('s3://athena-results-dforsber/iceberg_table/metadata/*.json', filename=1) ORDER BY "last-sequence-number" DESC LIMIT 1 );
+-- 1. Pick the latest Iceberg Table version
+SET VARIABLE manifest_list_avro_file = (SELECT snapshots[len(snapshots)]['manifest-list'] AS path FROM read_json(getvariable('metadata_json_path')));
+-- 2. snapshot manifest-list Avro file
+SET VARIABLE manifests = ( SELECT list(manifest_path) AS path FROM read_avro(getvariable('manifest_list')));
+-- 3. the actual manifest Avro file
+SET VARIABLE data_files = ( SELECT list(data_file['file_path']) AS path FROM read_avro(getvariable('manifests')) );
+-- 4. read the table data (latest version 2)
+SELECT COUNT(*) FROM parquet_scan(getvariable('data_files'));
